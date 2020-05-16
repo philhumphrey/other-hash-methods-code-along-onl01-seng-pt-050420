bf6d54184cd2c5ee3b5b4a203a8b5050af347a0b
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-16 06:21:30 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -17,5 +17,12 @@ groceries = {
   meat: ["chicken", "steak", "salmon"],
   grains: ["rice", "pasta"]
 }
-  
+  letters = {a: 1, b: 2}
+ 
+letters.min
+#  => [:a, 1]
+ 
+alphabetical_order = {c: 100, d: 1}
+alphabetical_order.min
+
 end
